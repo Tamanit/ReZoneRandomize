8ab9dc3e561676f94ec756e0bab124b5c2feaade
Plohih Alex. <plohih215@ya.ru>
2024-09-09 19:55:01 +0500

    setup db

--- api/src/Engine/Database/DbContext.cs
+++ api/src/Engine/Database/DbContext.cs
@@ -6,7 +6,7 @@ namespace api.DataBase;
 public sealed class AppContext: DbContext
 {
     public AppContext() => Database.EnsureCreated();
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("Host=db;Port=5432;Database=postgres;Username=root;Password=root");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("Host=srv-captain--rezone-database;Port=5432;Database=re_zone_random;Username=root;Password=root");
     
     public DbSet<BoardGameEntity> BoardGames { get; set; }
     public DbSet<BrunchEntity> Brunches { get; set; }
